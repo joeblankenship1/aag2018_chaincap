270a5249f6b5cf421b72fc7a3cc2f03f4c349bd6
joeblankenship1 <joe.blankenship1@gmail.com>
2018-03-26 03:00:19 -0400

    setup structure, modified index.html, created script.js for reveal init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+(function() {
+    // start reveal slideshow
+    Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
+
+        transition: 'convex', // none/fade/slide/convex/concave/zoom
+
+    });
+})();
