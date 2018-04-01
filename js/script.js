1ca41322ff8d6b4ea0c3016906d94f4c4ba81afa
joeblankenship1 <joe.blankenship1@gmail.com>
2018-04-01 07:28:29 -0400

    fix background ref issue

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
         transition: 'convex', // none/fade/slide/convex/concave/zoom
 
         // Parallax background image
-	    parallaxBackgroundImage: 'img/mining.jpg',
+	    parallaxBackgroundImage: 'https://raw.githubusercontent.com/joeblankenship1/aag2018_chaincap/master/img/mining.jpg',
 
 	    // Parallax background size
 	    parallaxBackgroundSize: '2100px 1080px',
